fab3ba297a8b23ba73e723a1c7203a0b17d06fdb
novanarifki <novanarifki02@gmail.com>
2025-01-05 22:10:57 +0700

    ubah informasi README

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,3 +168,22 @@ window.addEventListener("mousemove", function (event) {
   }
 
 });
+const emberCount = 100;
+function createEmber(){
+    const ember = document.createElement('div');
+    ember.classList.add('ember');
+    ember.style.left = '${Math.random() *100}vw';
+    const size = Math.randomm() * 10 + 10;
+    ember.style.width = '${size}px';
+    ember.style.height = '${size}px';
+    ember.style.animationDuration ='${Math.random() * 4 + 6}s';
+    ember.style.animationDelay = '${Math.random() * 2}s' ;
+    document.body.appendChild(ember);
+    ember.addEventListener('animationend', () => {
+        ember.remove();
+        createEmber();
+    });
+}
+for (let i = 0; i < emberCount; i++) {
+    createEmber();
+}
